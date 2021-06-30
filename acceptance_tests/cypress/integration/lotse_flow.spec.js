d2d2e80ea61aef86414b7e057c5cffdb4778fa3d
Frederike Ramin <frederike.ramin@4germany.org>
2021-06-30 10:11:04 +0200

    Disable link to revocation if logged in

--- acceptance_tests/cypress/integration/lotse_flow.spec.js
+++ acceptance_tests/cypress/integration/lotse_flow.spec.js
@@ -105,17 +105,25 @@ context('Acceptance tests', () => {
         cy.visit('/')
     });
 
+    it('Check that registration and revocation are clickable', () => {
+        cy.get('a').contains(/^Registrieren$/)
+            .should('have.class', 'nav-link')
+            .should('not.have.class', 'inactive')
+        cy.get('a').contains(/^Freischaltcode Stornierung$/)
+            .should('not.have.class', 'inactive')
+    })
+
     context('When I am logged in', () => {
         beforeEach(() => {
             login()
         })
 
-        it('Check that register and revocation are disabled', () => {
-            cy.visit('/')
-            cy.get('.nav-link').contains('Registrieren')
+        it('Check that registration and revocation are disabled', () => {
+            cy.get('span').contains(/^Registrieren$/)
+                .should('have.class', 'nav-link')
                 .should('have.class', 'inactive')
-                .click()
-            cy.location('pathname').should('eq', '/')
+            cy.get('span').contains(/^Freischaltcode Stornierung$/)
+                .should('have.class', 'inactive')
         })
 
         it('Enter different familienstands', () => {
