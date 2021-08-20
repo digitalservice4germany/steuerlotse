ca4d001e560f57f841aabe47814e1ba46cf58d43
Julian Roesner <julian.roesner@4germany.org>
2021-08-20 18:20:55 +0200

    Use common_vorsatz_args to make differnce more explicit

--- erica_app/erica/request_processing/requests_controller.py
+++ erica_app/erica/request_processing/requests_controller.py
@@ -88,9 +88,7 @@ class EstValidationRequestController(TransferTicketRequestController):
         # the Elster specification (see `Jahresdokumentation_10_2020.xml`)
         fields = est_mapping.check_and_generate_entries(self.input_data.est_data.__dict__)
 
-        if self.input_data.est_data.submission_without_tax_nr:
-            empfaenger = self.input_data.est_data.bufa_nr
-            vorsatz = generate_vorsatz_without_tax_number(
+        common_vorsatz_args = (
                 self.input_data.meta_data.year,
                 self.input_data.est_data.person_a_idnr,
                 self.input_data.est_data.person_b_idnr,
@@ -99,23 +97,17 @@ class EstValidationRequestController(TransferTicketRequestController):
                 self.input_data.est_data.person_a_street,
                 self.input_data.est_data.person_a_street_number,
                 self.input_data.est_data.person_a_plz,
-                self.input_data.est_data.person_a_town)
+                self.input_data.est_data.person_a_town
+        )
+        if self.input_data.est_data.submission_without_tax_nr:
+            empfaenger = self.input_data.est_data.bufa_nr
+            vorsatz = generate_vorsatz_without_tax_number(*common_vorsatz_args)
         else:
             electronic_steuernummer = est_mapping.generate_electronic_steuernummer(
                 self.input_data.est_data.steuernummer,
                 self.input_data.est_data.bundesland,
                 use_testmerker=self._is_testmerker_used())
-            vorsatz = generate_vorsatz_with_tax_number(
-                electronic_steuernummer,
-                self.input_data.meta_data.year,
-                self.input_data.est_data.person_a_idnr,
-                self.input_data.est_data.person_b_idnr,
-                self.input_data.est_data.person_a_first_name,
-                self.input_data.est_data.person_a_last_name,
-                self.input_data.est_data.person_a_street,
-                self.input_data.est_data.person_a_street_number,
-                self.input_data.est_data.person_a_plz,
-                self.input_data.est_data.person_a_town)
+            vorsatz = generate_vorsatz_with_tax_number(electronic_steuernummer, *common_vorsatz_args)
             empfaenger = electronic_steuernummer[:4]
 
         xml = elster_xml_generator.generate_full_est_xml(fields,
