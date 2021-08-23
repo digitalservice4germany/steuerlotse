f61ccca9418a0d88c8c9b50cffede346d11ce13d
Julian Roesner <julian.roesner@4germany.org>
2021-08-23 16:04:28 +0200

    Combine similar xml parsing functions

--- erica_app/erica/elster_xml/xml_parsing/erica_xml_parsing.py
+++ erica_app/erica/elster_xml/xml_parsing/erica_xml_parsing.py
@@ -16,36 +16,27 @@ def remove_declaration_and_namespace(xml_string):
     return ET.fromstring(xml_string)
 
 
-def get_elements_from_xml(xml_string, element):
-    xml_tree = remove_declaration_and_namespace(xml_string)
-    matched_elements = xml_tree.findall('.//' + element)
+def get_elements_from_xml_element(xml_element, tag_name):
+    matched_elements = xml_element.findall('.//' + tag_name)
     result = []
     for el in matched_elements:
         result.append(el)
-    if xml_tree.tag == element:  # also consider the element itself
-        result.append(xml_tree)
+    if xml_element.tag == tag_name:  # also consider the element itself
+        result.append(xml_element)
     return result
+
+
+def get_elements_from_xml(xml_string, element):
+    xml_tree = remove_declaration_and_namespace(xml_string)
+    return get_elements_from_xml_element(xml_tree, element)
 
 
 def get_elements_text_from_xml_element(xml_element, searched_child_element):
-    matched_elements = xml_element.findall('.//' + searched_child_element)
-    result = []
-    for el in matched_elements:
-        result.append(el.text)
-    if xml_element.tag == searched_child_element:  # also consider the element itself
-        result.append(xml_element.text)
-    return result
+    return [el.text for el in get_elements_from_xml_element(xml_element, searched_child_element)]
 
 
 def get_elements_text_from_xml(xml_string, element):
-    xml_tree = remove_declaration_and_namespace(xml_string)
-    matched_elements = xml_tree.findall('.//' + element)
-    result = []
-    for el in matched_elements:
-        result.append(el.text)
-    if xml_tree.tag == element:  # also consider the element itself
-        result.append(xml_tree.text)
-    return result
+    return [el.text for el in get_elements_from_xml(xml_string, element)]
 
 
 def get_elements_key_value_from_xml(input_xml, element, key):
