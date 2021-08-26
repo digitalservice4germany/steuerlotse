1a88cab6a7c24e92075b450842eb48851d46691c
Niko Felger <niko.felger@gmail.com>
2021-08-26 09:46:03 +0200

    Use .env file to set environment variables in dev

---
+++ webapp/autoapp.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+# TODO: this will be where the app instance is created for running the app
+# from app.app import create_app
+# app = create_app()
+
+from app import app
