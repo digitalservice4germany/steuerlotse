f0552639818a762a9bb411179b648f887a514bf3
Niko Felger <niko.felger@gmail.com>
2021-09-01 15:23:40 +0200

    Add FormHeader component

--- webapp/client/src/pages/LoginPage.js
+++ webapp/client/src/pages/LoginPage.js
@@ -1,15 +1,31 @@
 import PropTypes from "prop-types";
+import React from "react";
+import FormHeader from "../components/FormHeader";
 import StepHeaderButtons from "../components/StepHeaderButtons";
 
-export default function LoginPage({ prevUrl, backLinkText }) {
+export default function LoginPage({
+  prevUrl,
+  backLinkText,
+  stepTitle,
+  stepIntro,
+}) {
   return (
-    <StepHeaderButtons backLinkUrl={prevUrl} backLinkText={backLinkText} />
+    <React.Fragment>
+      <StepHeaderButtons backLinkUrl={prevUrl} backLinkText={backLinkText} />
+      <FormHeader title={stepTitle} intro={stepIntro} />
+    </React.Fragment>
   );
 }
 
 LoginPage.propTypes = {
+  // render_info.prev_url
   prevUrl: PropTypes.string,
+  // render_info.back_link_text
   backLinkText: PropTypes.string,
+  // render_info.step_title
+  stepTitle: PropTypes.string,
+  // render_info.step_intro
+  stepIntro: PropTypes.string,
 };
 
 LoginPage.defaultProps = {
