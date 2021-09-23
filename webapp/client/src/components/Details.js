6208e0236a9be90a4509df9e673f8b8db1bb9728
Niko Felger <niko.felger@gmail.com>
2021-09-23 21:37:09 +0200

    Reimplement Details using react-bootstrap

--- webapp/client/src/components/Details.js
+++ webapp/client/src/components/Details.js
@@ -1,8 +1,7 @@
-// TODO: get rid of bootstrap (or use react-bootstrap, reactstrap, etc.)
-import "bootstrap"; // For the data-toggle=collapse functionality
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Collapse from "react-bootstrap/Collapse";
 
 import downArrow from "../assets/icons/details_arrow_down.svg";
 import rightArrow from "../assets/icons/details_arrow_right.svg";
@@ -35,7 +34,9 @@ const DetailsCard = styled.div`
   }
 
   &.details-card .card-header span {
-    color: var(--text-color);
+    display: inline-block;
+    padding: 0 var(--spacing-01);
+    height: var(--lineheight-default);
     font-size: var(--text-medium);
     font-weight: var(--font-bold);
   }
@@ -55,142 +56,94 @@ const DetailsCard = styled.div`
     box-shadow: none;
   }
 
-  &.details-card .card-header .collapsed span {
+  &.details-card .card-header [aria-expanded="false"] {
     color: var(--link-color);
   }
 
-  &.details-card .card-header .collapsed span:hover {
+  &.details-card .card-header [aria-expanded="false"] :hover {
     color: var(--link-hover-color);
   }
 
   &.details-card .card-header button:focus span {
     color: var(--focus-text-color);
-
     background: var(--focus-color);
-
-    outline: none;
-    box-shadow: none;
-    border: 0;
   }
 
-  &.details-card .card-header .details-icon {
-    width: 14px;
-    min-width: 14px;
+  &.details-card .details-icon {
+    width: 12px;
+    min-width: 12px;
     height: var(--lineheight-default);
     padding: 0;
-    margin-top: 0.375rem;
-    background: url(${downArrow}) no-repeat left;
+    margin-right: var(--spacing-02);
   }
 
-  &.details-card .card-header .collapsed:hover .details-icon {
+  &.details-card [aria-expanded="false"]:hover .details-icon {
     background: url(${rightArrowHover}) no-repeat left;
   }
 
-  &.details-card .card-header .collapsed button:focus .details-icon {
+  &.details-card [aria-expanded="false"] button:focus .details-icon {
     background: url(${rightArrowFocus}) no-repeat left;
   }
 
-  &.details-card .card-header .collapsed .details-icon {
+  &.details-card [aria-expanded="false"] .details-icon {
     background: url(${rightArrow}) no-repeat left;
+  }
+
+  &.details-card [aria-expanded="true"] .details-icon {
+    background: url(${downArrow}) no-repeat left;
   }
 
   &.details-card .card-body {
     border: 0;
-  }
-
-  &.details-card .details-content {
-    padding: 1em 1em 0 0;
-  }
-
-  &.card-body-less-padding {
-    padding: 1em 1em var(--spacing-01) var(--spacing-01);
-  }
-
-  &.details-card .card-body {
     padding: 0 0 var(--spacing-04) 0;
   }
 
   &.details-card .card-body .block-quote-line {
     width: 2px;
     min-width: 2px;
-    margin: 0 var(--spacing-03) 0 5px;
+    margin: 0 var(--spacing-03) 0 4px;
     background-color: var(--border-color);
   }
 
-  &.details-card .card-header button:hover {
-    color: var(--link-hover-color);
-    outline: none;
+  &.details-card .details-content {
+    padding: var(--spacing-02) 1em var(--spacing-02) 0;
   }
 
-  &.details-card .card-header:not(.collapsed) .control-show-more {
-    /* We need to still show it to keep focus on the element */
-    height: 1px;
-    width: 1px;
-    min-width: 1px;
-    background-color: inherit;
-    padding: 0;
-    margin: 0;
-    overflow: hidden;
-    position: absolute;
-  }
-
-  &.details-card .card-header.collapsed .control-show-less {
-    /* We need to still show it to keep focus on the element */
-    height: 1px;
-    width: 1px;
-    min-width: 1px;
-    background-color: inherit;
-    padding: 0;
-    margin: 0;
-    overflow: hidden;
-    position: absolute;
+  &.details-card .details-content :last-child {
+    margin-bottom: 0;
   }
 `;
 
 function Details({ children, title, detailsId }) {
-  const outerDivId = `details-${detailsId}`;
+  const [open, setOpen] = useState(false);
+  const toggle = () => setOpen(!open);
+
   const detailsBodyId = `details-body-${detailsId}`;
-  const headingId = `heading-${detailsId}`;
+
   return (
-    <DetailsCard id={outerDivId} className="details-card ml-0 pl-0">
+    <DetailsCard className="details-card ml-0 pl-0">
       <div className="card">
         <div className="card-header unstyled-card-header d-sm-flex justify-content-between align-items-center cursor-pointer mb-0">
-          <div
-            className="collapsed w-100"
-            id={headingId}
-            data-toggle="collapse"
-            data-target={`#${detailsBodyId}`}
-            role="button"
-            aria-expanded="false"
+          <button
+            onClick={toggle}
+            className="w-100"
+            type="button"
+            aria-expanded={open}
             aria-controls={detailsBodyId}
           >
             <div className="row details-row">
-              <button
-                className="btn details-icon control-show-more"
-                type="button"
-                tabIndex="-1"
-                aria-hidden="true"
-              />
-              <button
-                className="btn details-icon control-show-less"
-                type="button"
-                tabIndex="-1"
-                aria-hidden="true"
-              />
-              <button className="btn" type="button">
-                <span className="mb-0">{title}</span>
-              </button>
+              <i className="details-icon" />
+              <span className="mb-0">{title}</span>
             </div>
-          </div>
+          </button>
         </div>
-        <div
-          id={detailsBodyId}
-          className="collapse"
-          aria-labelledby={headingId}
-          data-parent={`#${outerDivId}`}
-        >
-          <div className="card-body container pt-0">
-            <div className="details-row row">
+        <Collapse in={open}>
+          <div
+            id={detailsBodyId}
+            aria-labelledby="heading-idnr"
+            className="card-body pt-0"
+          >
+            <div className="row details-row">
               <div className="block-quote-line" />
               <div className="details-content">
                 {children.paragraphs.map((paragraph, index) => (
@@ -212,7 +165,7 @@ function Details({ children, title, detailsId }) {
               </div>
             </div>
           </div>
-        </div>
+        </Collapse>
       </div>
     </DetailsCard>
   );
