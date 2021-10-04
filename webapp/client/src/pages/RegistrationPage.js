ecd89f5c21242f53ebc2f1b23ca802811b122788
Niko Felger <niko.felger@gmail.com>
2021-10-04 17:04:33 +0200

    Fix eslint warning

--- webapp/client/src/pages/RegistrationPage.js
+++ webapp/client/src/pages/RegistrationPage.js
@@ -79,8 +79,8 @@ export default function RegistrationPage({
                 // The anchors get content in the translation file
                 // eslint-disable-next-line jsx-a11y/anchor-has-content
                 dataPrivacyLink: <a href={dataPrivacyLink} />,
-                // eslint-disable-next-line jsx-a11y/anchor-has-content
                 taxGdprLink: (
+                  // eslint-disable-next-line jsx-a11y/anchor-has-content
                   <a
                     href="https://www.bundesfinanzministerium.de/Content/DE/Downloads/BMF_Schreiben/Weitere_Steuerthemen/Abgabenordnung/2020-07-01-Korrektur-Allgemeine-Informationen-Datenschutz-Grundverordnung-Steuerverwaltung-anlage-1.pdf?__blob=publicationFile&v=3"
                     rel="noreferrer"
