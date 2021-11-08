d74a668c7e956f3d971670cadf32c2297f7c2b4d
Sanny Nguyen Hung <sanny.nguyen@4germany.org>
2021-11-08 14:41:11 +0100

    Another try

--- webapp/tasks.py
+++ webapp/tasks.py
@@ -84,6 +84,7 @@ def test(c):
 
 @task
 def lint(c):
+    c.run("pip freeze")
     # fail the build if there are Python syntax errors or undefined names
     c.run("flake8 . --count --select=E9,E112,E113,E117,E711,E713,E714,F63,F7,F82 --show-source --statistics")
     # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
