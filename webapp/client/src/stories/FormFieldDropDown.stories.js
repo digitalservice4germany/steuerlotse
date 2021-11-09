561a74c43598997e38b2259e63d102747616725d
Julian Roesner <julian.roesner@4germany.org>
2021-11-09 22:33:55 +0100

    Use Default.args

--- webapp/client/src/stories/FormFieldDropDown.stories.js
+++ webapp/client/src/stories/FormFieldDropDown.stories.js
@@ -23,7 +23,7 @@ Default.args = {
     text: "Bundesland",
   },
   errors: [],
-  values: [
+  options: [
     ["bw", "Baden-Württemberg"],
     ["by", "Bayern"],
     ["hh", "Hamburg"],
@@ -33,73 +33,28 @@ Default.args = {
 
 export const WithDetails = Template.bind({});
 WithDetails.args = {
-  fieldId: "dropDown",
-  fieldName: "dropDown",
-  label: {
-    text: "Bundesland",
-  },
-  errors: [],
+  ...Default.args,
   details: {
     title: "Warum muss ich das auswählen?",
     text: "Die Finanzämter sind nach Bundesländern sortiert.",
   },
-  values: [
-    ["bw", "Baden-Württemberg"],
-    ["by", "Bayern"],
-    ["hh", "Hamburg"],
-    ["he", "Hessen"],
-  ],
 };
 
 export const PreSelection = Template.bind({});
 PreSelection.args = {
-  fieldId: "dropDown",
-  fieldName: "dropDown",
-  label: {
-    text: "Bundesland",
-  },
-  errors: [],
-  values: [
-    ["bw", "Baden-Württemberg"],
-    ["by", "Bayern"],
-    ["hh", "Hamburg"],
-    ["he", "Hessen"],
-  ],
+  ...Default.args,
   preselectedValue: "by",
 };
 
 export const DefaultDropDownValue = Template.bind({});
 DefaultDropDownValue.args = {
-  fieldId: "dropDown",
-  fieldName: "dropDown",
+  ...Default.args,
   defaultValue: "Bitte auswählen",
-  label: {
-    text: "Bundesland",
-  },
-  errors: [],
-  values: [
-    ["bw", "Baden-Württemberg"],
-    ["by", "Bayern"],
-    ["hh", "Hamburg"],
-    ["he", "Hessen"],
-  ],
 };
 
 export const WithChangeHandler = Template.bind({});
 WithChangeHandler.args = {
-  fieldId: "dropDown",
-  fieldName: "dropDown",
-  defaultValue: "Bitte auswählen",
-  label: {
-    text: "Bundesland",
-  },
-  errors: [],
-  values: [
-    ["bw", "Baden-Württemberg"],
-    ["by", "Bayern"],
-    ["hh", "Hamburg"],
-    ["he", "Hessen"],
-  ],
+  ...Default.args,
   onChangeHandler: (event) => {
     alert(`Sie leben in ${event.target.selectedOptions[0].text}.`);
   },
