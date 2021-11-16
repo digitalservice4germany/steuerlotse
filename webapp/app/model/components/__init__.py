20953b01698a3f4f3be2fcdbe32c329793fc4717
Julian Roesner <julian.roesner@4germany.org>
2021-11-16 10:35:21 +0100

    Add Props

--- webapp/app/model/components/__init__.py
+++ webapp/app/model/components/__init__.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Union, Tuple
 
 import humps
 from pydantic import BaseModel, Extra
@@ -31,7 +31,23 @@ class CheckboxFieldProps(ComponentProps):
     errors: List[str]
 
 
-FieldProps = Union[InputFieldProps, CheckboxFieldProps]
+class SelectOption(BaseModel, extra=Extra.forbid):
+    value: str
+    display_name: str
+
+
+class SelectFieldProps(ComponentProps):
+    selected_value: Optional[str]
+    choices: List[SelectOption]
+    errors: List[str]
+
+
+class YesNoFieldProps(ComponentProps):
+    value: Optional[str]
+    errors: List[str]
+
+
+FieldProps = Union[SelectFieldProps, InputFieldProps, CheckboxFieldProps, YesNoFieldProps]
 
 
 class StepFormProps(ComponentProps):
@@ -55,8 +71,13 @@ class DeclarationIncomesProps(StepFormProps):
     pass
 
 
-class SelectStmindProps(StepFormProps):
-    prev_url: str  # TODO place this at a more general place when implementing more StepFormProps for the LotseFlow
+class LotseStepFormProps(StepFormProps):
+    prev_url: str
+
+
+class TaxNumberStepFormProps(LotseStepFormProps):
+    tax_office_list: List[Dict]
+    number_of_users: int
 
 
 class ConfirmationProps(StepFormProps):
