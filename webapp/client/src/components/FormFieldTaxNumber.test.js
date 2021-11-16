0f1118df8ea7ccf3144fe5e8780f6dc3c959587e
Julian Roesner <julian.roesner@4germany.org>
2021-11-16 16:24:58 +0100

    Adapt test messages

--- webapp/client/src/components/FormFieldTaxNumber.test.js
+++ webapp/client/src/components/FormFieldTaxNumber.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { fireEvent } from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
 import FormFieldTaxNumber from "./FormFieldTaxNumber";
 
@@ -23,15 +22,15 @@ describe("FormFieldTaxNumber is split", () => {
     render(<FormFieldTaxNumber {...props} />);
   });
 
-  it("should show three input fields", () => {
+  it("Should show three input fields", () => {
     expect(screen.getAllByRole("textbox")).toHaveLength(3);
   });
 
-  it("Do not show example input", () => {
+  it("Should not show example input", () => {
     expect(screen.queryByText("exampleInput")).toBeNull();
   });
 
-  describe("Type 12 characters into each input", () => {
+  describe("When typing 12 characters into each input", () => {
     const input_characters = "123456789012";
 
     beforeEach(() => {
@@ -40,19 +39,19 @@ describe("FormFieldTaxNumber is split", () => {
       userEvent.type(screen.getAllByRole("textbox")[2], input_characters);
     });
 
-    it("First input contains only first 3 characters", () => {
+    it("First input should only contain first 3 characters", () => {
       expect(screen.getAllByRole("textbox")[0]).toHaveValue(
         input_characters.slice(0, 3)
       );
     });
 
-    it("First input contains only first 4 characters", () => {
+    it("Second input should only contain first 4 characters", () => {
       expect(screen.getAllByRole("textbox")[1]).toHaveValue(
         input_characters.slice(0, 4)
       );
     });
 
-    it("First input contains only first 4 characters", () => {
+    it("Third input should only contain first 4 characters", () => {
       expect(screen.getAllByRole("textbox")[2]).toHaveValue(
         input_characters.slice(0, 4)
       );
@@ -64,7 +63,7 @@ describe("FormFieldTaxNumber is split", () => {
       userEvent.tab();
     });
 
-    it("Set focus on first input field", () => {
+    it("Should focus on first input field", () => {
       expect(screen.getAllByRole("textbox")[0]).toHaveFocus();
     });
 
@@ -75,19 +74,19 @@ describe("FormFieldTaxNumber is split", () => {
         userEvent.keyboard(input_characters);
       });
 
-      it("Enter letters into first input field", () => {
+      it("Should enter letters into first input field", () => {
         expect(screen.getAllByRole("textbox")[0]).toHaveValue(
           input_characters.slice(0, 3)
         );
       });
     });
 
-    it("Set focus on second input field after second tab", () => {
+    it("Should set focus on second input field after second tab", () => {
       userEvent.tab();
       expect(screen.getAllByRole("textbox")[1]).toHaveFocus();
     });
 
-    it("Set focus on third input field after third tab", () => {
+    it("Should set focus on third input field after third tab", () => {
       userEvent.tab();
       userEvent.tab();
       expect(screen.getAllByRole("textbox")[2]).toHaveFocus();
@@ -114,22 +113,22 @@ describe("FormFieldTaxNumber is not split", () => {
     render(<FormFieldTaxNumber {...props} />);
   });
 
-  it("Show only one input field", () => {
+  it("Should only show one input field", () => {
     expect(screen.getAllByRole("textbox")).toHaveLength(1);
   });
 
-  it("show example input", () => {
+  it("Should show example input", () => {
     expect(screen.queryByText("exampleInput")).toBeNull();
   });
 
-  describe("Type 12 characters into input", () => {
+  describe("When typing 12 characters into input", () => {
     const input_characters = "123456789012";
 
     beforeEach(() => {
       userEvent.type(screen.getByRole("textbox"), input_characters);
     });
 
-    it("Input contains only first 11 characters", () => {
+    it("Input should contain only first 11 characters", () => {
       expect(screen.getByRole("textbox")).toHaveValue(
         input_characters.slice(0, 11)
       );
@@ -141,7 +140,7 @@ describe("FormFieldTaxNumber is not split", () => {
       userEvent.tab();
     });
 
-    it("Set focus on input field", () => {
+    it("Should set focus on input field", () => {
       expect(screen.getByRole("textbox")).toHaveFocus();
     });
 
@@ -152,7 +151,7 @@ describe("FormFieldTaxNumber is not split", () => {
         userEvent.keyboard(input_characters);
       });
 
-      it("Enter letters into input field", () => {
+      it("Should enter letters into input field", () => {
         expect(screen.getByRole("textbox")).toHaveValue(
           input_characters.slice(0, 11)
         );
