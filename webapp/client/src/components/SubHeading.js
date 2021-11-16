f2a318be5feac1c168e9ab905d3eae363845a990
Julian Roesner <julian.roesner@4germany.org>
2021-11-16 10:33:34 +0100

    Add SubHeading component

---
+++ webapp/client/src/components/SubHeading.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledSubHeading = styled.h2`
+  & {
+    font-size: var(--text-medium);
+    margin-top: var(--spacing-09);
+    margin-bottom: var(--spacing-03);
+  }
+`;
+
+export default StyledSubHeading;
