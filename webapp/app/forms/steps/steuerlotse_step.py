31cc4272319b26e8f02b1bddccdd46bbcb3a2ceb
Frederike Ramin <frederike.ramin@4germany.org>
2021-11-29 08:30:53 +0100

    Update render_info.stored_data instead of stored_data

--- webapp/app/forms/steps/steuerlotse_step.py
+++ webapp/app/forms/steps/steuerlotse_step.py
@@ -141,13 +141,11 @@ class FormSteuerlotseStep(SteuerlotseStep):
     @classmethod
     def prepare_render_info(cls, stored_data, input_data=None, should_update_data=False, *args, **kwargs):
         render_info = super().prepare_render_info(stored_data, input_data, should_update_data, *args, **kwargs)
-        render_info.form = cls.create_form(form_data=input_data, prefilled_data=stored_data)
+        render_info.form = cls.create_form(form_data=input_data, prefilled_data=render_info.stored_data)
 
         if should_update_data and render_info.form.validate():
             render_info.data_is_valid = True
-            stored_data.update(render_info.form.data)
-
-        render_info.stored_data = stored_data
+            render_info.stored_data.update(render_info.form.data)
 
         return render_info
 
