7106e7edee5b3d4b64e3841e1a7d775aadef029d
Frederike Ramin <9320199+frederike-ramin@users.noreply.github.com>
2021-12-01 16:03:17 +0100

    Reactify text input field (#424)
    * Add TextInput class plus stories
    
    * Add tests for text input field
    
    * Introduce maxWidth instead of setMaxLength
    
    * Add keyboard tests + restructure

---
+++ webapp/client/src/components/FormFieldTextInput.js
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import FormFieldScaffolding from "./FormFieldScaffolding";
+import FieldLabel from "./FieldLabel";
+import FormFieldSeparatedField from "./FormFieldSeparatedField";
+
+function FormFieldTextInput({
+  fieldName,
+  fieldId,
+  value,
+  required,
+  autofocus,
+  label,
+  maxWidth,
+  maxLength,
+  details,
+  errors,
+}) {
+  return (
+    <FormFieldScaffolding
+      {...{
+        fieldName,
+        errors,
+      }}
+      labelComponent={<FieldLabel {...{ label, fieldId, details }} />}
+      render={() => (
+        <input
+          type="text"
+          id={fieldId}
+          name={fieldName}
+          defaultValue={value}
+          maxLength={maxLength}
+          // TODO: autofocus is under review.
+          // eslint-disable-next-line
+          autoFocus={autofocus || Boolean(errors.length)}
+          required={required}
+          className={classNames("form-control", `input-width-${maxWidth}`)}
+        />
+      )}
+    />
+  );
+}
+
+FormFieldTextInput.propTypes = {
+  fieldId: PropTypes.string.isRequired,
+  fieldName: PropTypes.string.isRequired,
+  errors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  autofocus: PropTypes.bool,
+  required: PropTypes.bool,
+  value: PropTypes.string.isRequired,
+  label: FieldLabel.propTypes.label,
+  maxWidth: PropTypes.number,
+  maxLength: PropTypes.number,
+  details: FieldLabel.propTypes.details,
+};
+
+FormFieldTextInput.defaultProps = {
+  autofocus: FormFieldSeparatedField.defaultProps.autofocus,
+  required: FormFieldSeparatedField.defaultProps.required,
+  label: FieldLabel.defaultProps.label,
+  maxWidth: 25,
+  maxLength: undefined,
+  details: FieldLabel.defaultProps.details,
+};
+
+export default FormFieldTextInput;
