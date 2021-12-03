8a054adab630dfd55dc9f6f1a973de805a95f27e
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-03 16:02:35 +0100

    Add integration tests

---
+++ webapp/client/cypress/integration/telephoneNumber.spec.js
@@ -0,0 +1,52 @@
+describe("TelephoneNumber", () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  context("with no data", () => {
+    beforeEach(() => {
+      cy.visit("/lotse/step/telephone_number");
+    });
+
+    it("Should link back to person a page", () => {
+      cy.get("a").contains("Zurück").click();
+      cy.url().should("include", "/lotse/step/person_a");
+    });
+
+    it("Should link forward to iban page", () => {
+      cy.get("button[type=submit]").click();
+      cy.url().should("include", "/lotse/step/iban");
+    });
+  });
+
+  context("for separated taxes", () => {
+    beforeEach(() => {
+      cy.request("POST", "/testing/set_data/form_data", {
+        familienstand: "single",
+      });
+      cy.visit("/lotse/step/telephone_number");
+    });
+
+    it("Should link back to person a page", () => {
+      cy.get("a").contains("Zurück").click();
+      cy.url().should("include", "/lotse/step/person_a");
+    });
+  });
+
+  context("for joint taxes", () => {
+    beforeEach(() => {
+      cy.request("POST", "/testing/set_data/form_data", {
+        familienstand: "married",
+        familienstand_date: "02.09.2022",
+        familienstand_married_lived_separated: "no",
+        familienstand_confirm_zusammenveranlagung: true,
+      });
+      cy.visit("/lotse/step/telephone_number");
+    });
+
+    it("Should link back to person b page", () => {
+      cy.get("a").contains("Zurück").click();
+      cy.url().should("include", "/lotse/step/person_b");
+    });
+  });
+});
