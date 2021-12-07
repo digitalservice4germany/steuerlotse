f2b4360c27e985d192b127cde9467bb4132e634e
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-07 14:44:01 +0100

    Add beh_grad 20

--- webapp/app/forms/steps/lotse/personal_data.py
+++ webapp/app/forms/steps/lotse/personal_data.py
@@ -245,7 +245,7 @@ class StepPersonA(LotseFormSteuerlotseStep):
         person_a_beh_grad = SteuerlotseIntegerField(
             label=_l('form.lotse.field_person_beh_grad'),
             validators=[
-                validators.any_of([25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100])],
+                validators.any_of([20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100])],
             render_kw={'help': _l('form.lotse.field_person_beh_grad-help'),
                        'data_label': _l('form.lotse.field_person_beh_grad.data_label'),
                        'data-example-input': _l('form.lotse.field_person_beh_grad.example_input'),
@@ -376,7 +376,7 @@ class StepPersonB(LotseFormSteuerlotseStep):
 
         person_b_beh_grad = SteuerlotseIntegerField(
             label=_l('form.lotse.field_person_beh_grad'),
-            validators=[validators.any_of([25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100])],
+            validators=[validators.any_of([20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100])],
             render_kw={'help': _l('form.lotse.field_person_beh_grad-help'),
                        'data_label': _l('form.lotse.field_person_beh_grad.data_label'),
                        'data-example-input': _l('form.lotse.field_person_beh_grad.example_input'),
