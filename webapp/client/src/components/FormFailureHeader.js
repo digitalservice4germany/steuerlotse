3c267346de653df809baf676baeb939bfd37df5c
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-08 08:40:08 +0100

    Add failure error

---
+++ webapp/client/src/components/FormFailureHeader.js
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const FailureAlertArea = styled.div`
+  &.alert {
+    border-radius: 0;
+    border: 0;
+    padding: var(--spacing-05);
+  }
+
+  &.alert-danger {
+    background-color: var(--error-color);
+    color: var(--inverse-text-color);
+  }
+`;
+
+const Intro = styled.p`
+  font-size: var(--text-medium);
+`;
+
+export default function FormFailureHeader({ title, intro, hideIntro }) {
+  return (
+    <>
+      <FailureAlertArea className="alert alert-danger" role="alert">
+        <h1>{title}</h1>
+      </FailureAlertArea>
+      {intro && !hideIntro && <Intro>{intro}</Intro>}
+    </>
+  );
+}
+
+FormFailureHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+  intro: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  hideIntro: PropTypes.bool,
+};
+
+FormFailureHeader.defaultProps = {
+  intro: undefined,
+  hideIntro: false,
+};
---
+++ webapp/client/src/stories/FormFailureHeader.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import FormFailureHeader from "../components/FormFailureHeader";
+
+export default {
+  title: "Forms/FailureHeader",
+  component: FormFailureHeader,
+};
+
+function Template(args) {
+  return <FormFailureHeader {...args} />;
+}
+
+export const Default = Template.bind({});
+Default.args = {
+  title: "Anmeldung fehlgeschlagen",
+  intro: "Ihr Freischaltcode konnte nicht validiert werden.",
+};
