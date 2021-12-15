db3633a1205d58bd08a2b54d7041a6403cd33b2b
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-15 09:10:47 +0100

    Add radio button field and story

---
+++ webapp/client/src/stories/FormFieldRadio.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import FormFieldRadio from "../components/FormFieldRadio";
+import StepForm from "../components/StepForm";
+import { Default as StepFormDefault } from "./StepForm.stories";
+
+export default {
+  title: "Form Fields/Radio",
+  component: FormFieldRadio,
+};
+
+function Template(args) {
+  return (
+    <StepForm {...StepFormDefault.args}>
+      <FormFieldRadio {...args} />
+    </StepForm>
+  );
+}
+
+export const Default = Template.bind({});
+Default.args = {
+  fieldId: "radio",
+  fieldName: "radio",
+  options: [
+    { label: "Ja, ich zahle oder beziehe Unterhalt.", value: "yes" },
+    { label: "Nein, weder zahle, noch beziehe ich Unterhalt.", value: "no" },
+  ],
+  label: {
+    text: "Zahlen oder beziehen Sie Unterhalt?",
+  },
+  errors: [],
+};
