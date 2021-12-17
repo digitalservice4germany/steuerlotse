6bebd780673d7f760a877420a42ac1a3d6e41f30
Julian <julian.roesner@4germany.org>
2021-12-17 12:48:49 +0100

    Add calculation for fahrkostenpauschbetrag (#493)

---
+++ webapp/app/forms/steps/lotse/fahrkostenpauschbetrag.py
@@ -0,0 +1,12 @@
+def calculate_fahrkostenpauschbetrag(has_pflegegrad=False, disability_degree=None, has_merkzeichen_bl=False, has_merkzeichen_tbl=False,
+                                     has_merkzeichen_h=False, has_merkzeichen_ag=False, has_merkzeichen_g=False):
+    if has_pflegegrad or has_merkzeichen_bl or has_merkzeichen_tbl or has_merkzeichen_tbl or has_merkzeichen_h or has_merkzeichen_ag:
+        return 4500
+
+    elif disability_degree >= 80:
+        return 900
+    elif has_merkzeichen_g and disability_degree >= 70:
+        return 900
+
+    else:
+        return None
