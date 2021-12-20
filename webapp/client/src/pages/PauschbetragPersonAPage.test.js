da202d0ef43069a268694194c896870367fc8121
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-20 14:32:02 +0100

    Add tests

---
+++ webapp/client/src/pages/PauschbetragPersonAPage.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import PauschbetragPersonAPage from "./PauschbetragPersonAPage";
+import { Default as StepFormDefault } from "../stories/StepForm.stories";
+
+let props = {
+  stepHeader: {
+    title: "Title",
+    intro: "Intro",
+  },
+  form: {
+    ...StepFormDefault.args,
+  },
+  fields: {
+    personAWantsPauschbetrag: {
+      selectedValue: undefined,
+      options: [
+        {
+          value: "yes",
+          displayName: "Ja",
+        },
+        {
+          value: "no",
+          displayName: "Nein",
+        },
+      ],
+      errors: [],
+    },
+  },
+  prevUrl: "/some/prev/path",
+};
+
+describe("With default props", () => {
+  beforeEach(() => {
+    render(<PauschbetragPersonAPage {...props} />);
+  });
+
+  it("should render step title text", () => {
+    expect(screen.getByText("Title")).toBeInTheDocument();
+  });
+
+  it("should ignore step intro text", () => {
+    expect(screen.queryByText("Intro")).not.toBeInTheDocument();
+  });
+
+  it("should render field", () => {
+    expect(screen.getByLabelText("Ja")).toBeInTheDocument();
+    expect(screen.getByLabelText("Nein")).toBeInTheDocument();
+  });
+
+  it("should show details", () => {
+    //expect(screen.getElementsByClassName('details-card').length).toBe(1);
+  });
+
+  it("should link to the previous page", () => {
+    expect(screen.getByText("Zurück").closest("a")).toHaveAttribute(
+      "href",
+      expect.stringContaining("/some/prev/path")
+    );
+  });
+});
---
+++ webapp/client/src/pages/PauschbetragPersonBPage.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import PauschbetragPersonBPage from "./PauschbetragPersonBPage";
+import { Default as StepFormDefault } from "../stories/StepForm.stories";
+
+let props = {
+  stepHeader: {
+    title: "Title",
+    intro: "Intro",
+  },
+  form: {
+    ...StepFormDefault.args,
+  },
+  fields: {
+    personBWantsPauschbetrag: {
+      selectedValue: undefined,
+      options: [
+        {
+          value: "yes",
+          displayName: "Ja",
+        },
+        {
+          value: "no",
+          displayName: "Nein",
+        },
+      ],
+      errors: [],
+    },
+  },
+  prevUrl: "/some/prev/path",
+};
+
+describe("With default props", () => {
+  beforeEach(() => {
+    render(<PauschbetragPersonBPage {...props} />);
+  });
+
+  it("should render step title text", () => {
+    expect(screen.getByText("Title")).toBeInTheDocument();
+  });
+
+  it("should ignore step intro text", () => {
+    expect(screen.queryByText("Intro")).not.toBeInTheDocument();
+  });
+
+  it("should render field", () => {
+    expect(screen.getByLabelText("Ja")).toBeInTheDocument();
+    expect(screen.getByLabelText("Nein")).toBeInTheDocument();
+  });
+
+  it("should show details", () => {
+    //expect(screen.getElementsByClassName('details-card').length).toBe(1);
+  });
+
+  it("should link to the previous page", () => {
+    expect(screen.getByText("Zurück").closest("a")).toHaveAttribute(
+      "href",
+      expect.stringContaining("/some/prev/path")
+    );
+  });
+});
