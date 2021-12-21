746246c0a4f1a1f5b5b0bdb2717bf40b7080b2d3
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-21 09:36:40 +0100

    Remove span from legend which makes legend inaccessible

--- webapp/client/src/components/FieldLabelForSeparatedFields.js
+++ webapp/client/src/components/FieldLabelForSeparatedFields.js
@@ -19,9 +19,7 @@ export default function FieldLabelForSeparatedFields(props) {
     <FieldLabelScaffolding
       {...props}
       render={(innerContent, className) => (
-        <span>
-          <Legend className={className}>{innerContent}</Legend>
-        </span>
+        <Legend className={className}>{innerContent}</Legend>
       )}
     />
   );
