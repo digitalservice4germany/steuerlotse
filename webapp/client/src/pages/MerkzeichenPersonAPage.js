287c6fd01335a7fb447865ddd92aa000b8c5cd4f
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-22 16:47:14 +0100

    Abstract person a and b into one common page

--- webapp/client/src/pages/MerkzeichenPersonAPage.js
+++ webapp/client/src/pages/MerkzeichenPersonAPage.js
@@ -1,14 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import FormHeader from "../components/FormHeader";
-import StepForm from "../components/StepForm";
-import StepHeaderButtons from "../components/StepHeaderButtons";
 import { checkboxPropType, fieldPropType } from "../lib/propTypes";
-import FormFieldYesNo from "../components/FormFieldYesNo";
-import FormFieldIntegerInput from "../components/FormFieldIntegerInput";
-import FormFieldCheckBox from "../components/FormFieldCheckBox";
-import FieldLabelForSeparatedFields from "../components/FieldLabelForSeparatedFields";
+import MerkzeichenPage from "./MerkzeichenPage";
 
 export default function MerkzeichenPersonAPage({
   stepHeader,
@@ -19,77 +13,61 @@ export default function MerkzeichenPersonAPage({
   const { t } = useTranslation();
 
   return (
-    <>
-      <StepHeaderButtons url={prevUrl} />
-      <FormHeader {...stepHeader} />
-      <StepForm {...form}>
-        <FormFieldYesNo
-          fieldName="person_a_has_pflegegrad"
-          fieldId="person_a_has_pflegegrad"
-          value={fields.personAHasPflegegrad.value}
-          label={{
+    <MerkzeichenPage
+      {...{ stepHeader, form, prevUrl }}
+      fields={{
+        hasPflegegrad: {
+          ...fields.personAHasPflegegrad,
+          name: "person_a_has_pflegegrad",
+          label: {
             text: t("lotse.merkzeichen.hasPflegegrad.label"),
-          }}
-          errors={fields.personAHasPflegegrad.errors}
-        />
-        <FormFieldIntegerInput
-          fieldName="person_a_disability_degree"
-          fieldId="person_a_disability_degree"
-          value={fields.personADisabilityDegree.value}
-          label={{
+          },
+        },
+        disabilityDegree: {
+          ...fields.personADisabilityDegree,
+          name: "person_a_disability_degree",
+          label: {
             text: t("lotse.merkzeichen.disabilityDegree.label"),
             exampleInput: t("lotse.merkzeichen.disabilityDegree.example"),
-          }}
-          fieldWidth={3}
-          errors={fields.personADisabilityDegree.errors}
-        />
-        <fieldset id="merkzeichen">
-          <FieldLabelForSeparatedFields
-            fieldId="merkzeichen"
-            label={{ text: t("lotse.merkzeichen.merkzeichen.label") }}
-            details={{
-              title: t("lotse.merkzeichen.merkzeichen.details.title"),
-              text: t("lotse.merkzeichen.merkzeichen.details.text"),
-            }}
-          />
-          <FormFieldCheckBox
-            fieldName="person_a_has_merkzeichen_g"
-            fieldId="person_a_has_merkzeichen_g"
-            checked={fields.personAHasMerkzeichenG.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenG.label")}
-            errors={fields.personAHasMerkzeichenG.errors}
-          />
-          <FormFieldCheckBox
-            fieldName="person_a_has_merkzeichen_ag"
-            fieldId="person_a_has_merkzeichen_ag"
-            checked={fields.personAHasMerkzeichenAg.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenAg.label")}
-            errors={fields.personAHasMerkzeichenAg.errors}
-          />
-          <FormFieldCheckBox
-            fieldName="person_a_has_merkzeichen_bl"
-            fieldId="person_a_has_merkzeichen_bl"
-            checked={fields.personAHasMerkzeichenBl.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenBl.label")}
-            errors={fields.personAHasMerkzeichenBl.errors}
-          />
-          <FormFieldCheckBox
-            fieldName="person_a_has_merkzeichen_tbl"
-            fieldId="person_a_has_merkzeichen_tbl"
-            checked={fields.personAHasMerkzeichenTbl.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenTbl.label")}
-            errors={fields.personAHasMerkzeichenTbl.errors}
-          />
-          <FormFieldCheckBox
-            fieldName="person_a_has_merkzeichen_h"
-            fieldId="person_a_has_merkzeichen_h"
-            checked={fields.personAHasMerkzeichenH.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenH.label")}
-            errors={fields.personAHasMerkzeichenH.errors}
-          />
-        </fieldset>
-      </StepForm>
-    </>
+          },
+        },
+        hasMerkzeichenG: {
+          ...fields.personAHasMerkzeichenG,
+          name: "person_a_has_merkzeichen_g",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenG.label"),
+          },
+        },
+        hasMerkzeichenAg: {
+          ...fields.personAHasMerkzeichenAg,
+          name: "person_a_has_merkzeichen_ag",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenAg.label"),
+          },
+        },
+        hasMerkzeichenBl: {
+          ...fields.personAHasMerkzeichenBl,
+          name: "person_a_has_merkzeichen_bl",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenBl.label"),
+          },
+        },
+        hasMerkzeichenTbl: {
+          ...fields.personAHasMerkzeichenTbl,
+          name: "person_a_has_merkzeichen_tbl",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenTbl.label"),
+          },
+        },
+        hasMerkzeichenH: {
+          ...fields.personAHasMerkzeichenH,
+          name: "person_a_has_merkzeichen_h",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenH.label"),
+          },
+        },
+      }}
+    />
   );
 }
 
--- webapp/client/src/pages/MerkzeichenPersonBPage.js
+++ webapp/client/src/pages/MerkzeichenPersonBPage.js
@@ -1,14 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import FormHeader from "../components/FormHeader";
-import StepForm from "../components/StepForm";
-import StepHeaderButtons from "../components/StepHeaderButtons";
 import { checkboxPropType, fieldPropType } from "../lib/propTypes";
-import FormFieldYesNo from "../components/FormFieldYesNo";
-import FormFieldIntegerInput from "../components/FormFieldIntegerInput";
-import FormFieldCheckBox from "../components/FormFieldCheckBox";
-import FieldLabelForSeparatedFields from "../components/FieldLabelForSeparatedFields";
+import MerkzeichenPage from "./MerkzeichenPage";
 
 export default function MerkzeichenPersonBPage({
   stepHeader,
@@ -19,77 +13,61 @@ export default function MerkzeichenPersonBPage({
   const { t } = useTranslation();
 
   return (
-    <>
-      <StepHeaderButtons url={prevUrl} />
-      <FormHeader {...stepHeader} />
-      <StepForm {...form}>
-        <FormFieldYesNo
-          fieldName="person_b_has_pflegegrad"
-          fieldId="person_b_has_pflegegrad"
-          value={fields.personBHasPflegegrad.value}
-          label={{
+    <MerkzeichenPage
+      {...{ stepHeader, form, prevUrl }}
+      fields={{
+        hasPflegegrad: {
+          ...fields.personBHasPflegegrad,
+          name: "person_b_has_pflegegrad",
+          label: {
             text: t("lotse.merkzeichen.hasPflegegrad.label"),
-          }}
-          errors={fields.personBHasPflegegrad.errors}
-        />
-        <FormFieldIntegerInput
-          fieldName="person_b_disability_degree"
-          fieldId="person_b_disability_degree"
-          value={fields.personBDisabilityDegree.value}
-          label={{
+          },
+        },
+        disabilityDegree: {
+          ...fields.personBDisabilityDegree,
+          name: "person_b_disability_degree",
+          label: {
             text: t("lotse.merkzeichen.disabilityDegree.label"),
             exampleInput: t("lotse.merkzeichen.disabilityDegree.example"),
-          }}
-          fieldWidth={3}
-          errors={fields.personBDisabilityDegree.errors}
-        />
-        <fieldset id="merkzeichen">
-          <FieldLabelForSeparatedFields
-            fieldId="merkzeichen"
-            label={{ text: t("lotse.merkzeichen.merkzeichen.label") }}
-            details={{
-              title: t("lotse.merkzeichen.merkzeichen.details.title"),
-              text: t("lotse.merkzeichen.merkzeichen.details.text"),
-            }}
-          />
-          <FormFieldCheckBox
-            fieldName="person_b_has_merkzeichen_g"
-            fieldId="person_b_has_merkzeichen_g"
-            checked={fields.personBHasMerkzeichenG.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenG.label")}
-            errors={fields.personBHasMerkzeichenG.errors}
-          />
-          <FormFieldCheckBox
-            fieldName="person_b_has_merkzeichen_ag"
-            fieldId="person_b_has_merkzeichen_ag"
-            checked={fields.personBHasMerkzeichenAg.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenAg.label")}
-            errors={fields.personBHasMerkzeichenAg.errors}
-          />
-          <FormFieldCheckBox
-            fieldName="person_b_has_merkzeichen_bl"
-            fieldId="person_b_has_merkzeichen_bl"
-            checked={fields.personBHasMerkzeichenBl.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenBl.label")}
-            errors={fields.personBHasMerkzeichenBl.errors}
-          />
-          <FormFieldCheckBox
-            fieldName="person_b_has_merkzeichen_tbl"
-            fieldId="person_b_has_merkzeichen_tbl"
-            checked={fields.personBHasMerkzeichenTbl.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenTbl.label")}
-            errors={fields.personBHasMerkzeichenTbl.errors}
-          />
-          <FormFieldCheckBox
-            fieldName="person_b_has_merkzeichen_h"
-            fieldId="person_b_has_merkzeichen_h"
-            checked={fields.personBHasMerkzeichenH.checked}
-            labelText={t("lotse.merkzeichen.hasMerkzeichenH.label")}
-            errors={fields.personBHasMerkzeichenH.errors}
-          />
-        </fieldset>
-      </StepForm>
-    </>
+          },
+        },
+        hasMerkzeichenG: {
+          ...fields.personBHasMerkzeichenG,
+          name: "person_b_has_merkzeichen_g",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenG.label"),
+          },
+        },
+        hasMerkzeichenAg: {
+          ...fields.personBHasMerkzeichenAg,
+          name: "person_b_has_merkzeichen_ag",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenAg.label"),
+          },
+        },
+        hasMerkzeichenBl: {
+          ...fields.personBHasMerkzeichenBl,
+          name: "person_b_has_merkzeichen_bl",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenBl.label"),
+          },
+        },
+        hasMerkzeichenTbl: {
+          ...fields.personBHasMerkzeichenTbl,
+          name: "person_b_has_merkzeichen_tbl",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenTbl.label"),
+          },
+        },
+        hasMerkzeichenH: {
+          ...fields.personBHasMerkzeichenH,
+          name: "person_b_has_merkzeichen_h",
+          label: {
+            text: t("lotse.merkzeichen.hasMerkzeichenH.label"),
+          },
+        },
+      }}
+    />
   );
 }
 
