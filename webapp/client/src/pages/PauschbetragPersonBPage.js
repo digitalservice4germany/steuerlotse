6d9c6e269bf9d480aba43efc18574b649b0eb699
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-23 15:41:15 +0100

    Use common component for person b page

--- webapp/client/src/pages/PauschbetragPersonBPage.js
+++ webapp/client/src/pages/PauschbetragPersonBPage.js
@@ -1,17 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { Trans, useTranslation } from "react-i18next";
-import styled from "styled-components";
-import FormHeader from "../components/FormHeader";
-import StepForm from "../components/StepForm";
-import StepHeaderButtons from "../components/StepHeaderButtons";
 import { selectionFieldPropType } from "../lib/propTypes";
-import FormFieldRadio, { boldifyChoices } from "../components/FormFieldRadio";
-import Details from "../components/Details";
-
-const DetailsDiv = styled.div`
-  margin-bottom: var(--spacing-04);
-`;
+import PauschbetragPage from "./PauschbetragPage";
 
 export default function PauschbetragPersonBPage({
   stepHeader,
@@ -19,61 +9,18 @@ export default function PauschbetragPersonBPage({
   fields,
   prevUrl,
 }) {
-  const { t } = useTranslation();
-  const boldChoices = boldifyChoices(fields.personBWantsPauschbetrag.options);
-
   return (
-    <>
-      <StepHeaderButtons url={prevUrl} />
-      <FormHeader
-        title={stepHeader.title}
-        intro={
-          <Trans
-            t={t}
-            i18nKey="lotse.pauschbetrag.intro"
-            components={{
-              bold: <b />,
-            }}
-          />
-        }
-      />
-      <DetailsDiv>
-        <Details
-          title={t("lotse.pauschbetrag.details.title")}
-          detailsId="pauschbetrag_person_a_details"
-        >
-          {[
-            <p key={0}>{t("lotse.pauschbetrag.details.p1")}</p>,
-            <ul key={1}>
-              <li key={0}>{t("lotse.pauschbetrag.details.list.listItem1")}</li>
-              <li key={1}>{t("lotse.pauschbetrag.details.list.listItem2")}</li>
-              <li key={2}>{t("lotse.pauschbetrag.details.list.listItem3")}</li>
-            </ul>,
-            <p key={2}>{t("lotse.pauschbetrag.details.p2")}</p>,
-            <p key={3}>
-              <Trans
-                t={t}
-                i18nKey="lotse.pauschbetrag.details.p3"
-                components={{
-                  bold: <b />,
-                }}
-              />
-            </p>,
-            <p key={4}>{t("lotse.pauschbetrag.details.p4")}</p>,
-          ]}
-        </Details>
-      </DetailsDiv>
-      <StepForm {...form}>
-        <FormFieldRadio
-          fieldName="person_a_wants_pauschbetrag"
-          fieldId="person_a_wants_pauschbetrag"
-          label={{ text: "" }}
-          options={boldChoices}
-          value={fields.personBWantsPauschbetrag.selectedValue}
-          errors={fields.personBWantsPauschbetrag.errors}
-        />
-      </StepForm>
-    </>
+    <PauschbetragPage
+      {...{ stepHeader, form, prevUrl }}
+      fields={{
+        wantsPauschbetrag: {
+          selectedValue: fields.personBWantsPauschbetrag.selectedValue,
+          errors: fields.personBWantsPauschbetrag.errors,
+          options: fields.personBWantsPauschbetrag.options,
+          name: "person_b_wants_pauschbetrag",
+        },
+      }}
+    />
   );
 }
 
