a4aa784ed4479f23e20084dbb7f50317dffd2c76
Frederike Ramin <frederike.ramin@4germany.org>
2021-12-28 11:56:53 +0100

    Remove unused import

--- webapp/client/src/pages/NoPauschbetragPage.test.js
+++ webapp/client/src/pages/NoPauschbetragPage.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import NoPauschbetragPage from "./NoPauschbetragPage";
-import { Default as StepFormDefault } from "../stories/StepForm.stories";
 
 let props = {
   stepHeader: {
