ffe166244cea2056a36af8b7fa5c23c7d8cdb27f
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-01-03 18:53:07 +0100

    Add merkzeichen data precondition

--- webapp/app/forms/steps/lotse/pauschbetrag.py
+++ webapp/app/forms/steps/lotse/pauschbetrag.py
@@ -1,9 +1,14 @@
+from typing import Optional
+
 from flask import render_template
 from flask_babel import lazy_gettext as _l, ngettext, _
-from wtforms.validators import InputRequired
+from pydantic import validator
+from pydantic.main import BaseModel
+from wtforms.validators import InputRequired, ValidationError
 from wtforms import SelectField
 from flask_wtf.csrf import generate_csrf
 
+from app.forms.steps.lotse.merkzeichen import StepMerkzeichenPersonA, StepMerkzeichenPersonB
 from app.model.components import PauschbetragProps
 from app.model.components.helpers import form_fields_dict
 from app.forms import SteuerlotseBaseForm
@@ -11,8 +16,7 @@ from app.forms.steps.step import SectionLink
 from app.forms.steps.lotse.lotse_step import LotseFormSteuerlotseStep
 from app.forms.steps.lotse.personal_data import ShowPersonBPrecondition, StepFamilienstand
 from app.forms.steps.lotse.utils import get_number_of_users
-from app.forms.steps.lotse.has_disability import PersonAHasDisabilityPrecondition, PersonBHasDisabilityPrecondition, \
-    HasDisabilityPersonAPrecondition, HasDisabilityPersonBPrecondition
+from app.forms.steps.lotse.has_disability import HasDisabilityPersonAPrecondition, HasDisabilityPersonBPrecondition
 
 
 def calculate_pauschbetrag(has_pflegegrad=False, disability_degree=None, has_merkzeichen_bl=False, has_merkzeichen_tbl=False, has_merkzeichen_h=False):
@@ -63,10 +67,36 @@ class StepPauschbetrag(LotseFormSteuerlotseStep):
         return result
 
 
+class HasMerkzeichenPersonAPrecondition(BaseModel):
+    _step_to_redirect_to = StepMerkzeichenPersonA.name
+    _message_to_flash = _l('form.lotse.skip_reason.has_no_merkzeichen')
+
+    person_a_has_pflegegrad: Optional[str]
+    person_a_disability_degree: Optional[int]
+    person_a_has_merkzeichen_g: Optional[bool]
+    person_a_has_merkzeichen_ag: Optional[bool]
+    person_a_has_merkzeichen_bl: Optional[bool]
+    person_a_has_merkzeichen_tbl: Optional[bool]
+    person_a_has_merkzeichen_h: Optional[bool]
+
+    @validator('person_a_has_merkzeichen_h', always=True)
+    def any_merkzeichen_has_to_be_set(cls, v, values):
+        merkzeichen_keys = ['person_a_has_pflegegrad',
+                            'person_a_disability_degree',
+                            'person_a_has_merkzeichen_g',
+                            'person_a_has_merkzeichen_ag',
+                            'person_a_has_merkzeichen_bl',
+                            'person_a_has_merkzeichen_tbl',
+                            'person_a_has_merkzeichen_h']
+        if not v and not any([values.get(merkzeichen_key) for merkzeichen_key in merkzeichen_keys]):
+            raise ValueError
+        return v
+
+
 class StepPauschbetragPersonA(StepPauschbetrag):
     name = 'person_a_requests_pauschbetrag'
     section_link = SectionLink('mandatory_data', StepFamilienstand.name, _l('form.lotse.mandatory_data.label'))
-    preconditions = [HasDisabilityPersonAPrecondition]
+    preconditions = [HasDisabilityPersonAPrecondition, HasMerkzeichenPersonAPrecondition]
     
     class InputForm(SteuerlotseBaseForm):
         person_a_requests_pauschbetrag = SelectField(
@@ -114,14 +144,40 @@ class StepPauschbetragPersonA(StepPauschbetrag):
             has_merkzeichen_tbl=self.stored_data.get('person_a_has_merkzeichen_tbl', False),
             has_merkzeichen_h=self.stored_data.get('person_a_has_merkzeichen_h', False)
         )
-        
+
+
+class HasMerkzeichenPersonBPrecondition(BaseModel):
+    _step_to_redirect_to = StepMerkzeichenPersonB.name
+    _message_to_flash = _l('form.lotse.skip_reason.has_no_merkzeichen')
+
+    person_b_has_pflegegrad: Optional[str]
+    person_b_disability_degree: Optional[int]
+    person_b_has_merkzeichen_g: Optional[bool]
+    person_b_has_merkzeichen_ag: Optional[bool]
+    person_b_has_merkzeichen_bl: Optional[bool]
+    person_b_has_merkzeichen_tbl: Optional[bool]
+    person_b_has_merkzeichen_h: Optional[bool]
+
+    @validator('person_b_has_merkzeichen_h', always=True)
+    def any_merkzeichen_has_to_be_set(cls, v, values):
+        merkzeichen_keys = ['person_b_has_pflegegrad',
+                            'person_b_disability_degree',
+                            'person_b_has_merkzeichen_g',
+                            'person_b_has_merkzeichen_ag',
+                            'person_b_has_merkzeichen_bl',
+                            'person_b_has_merkzeichen_tbl',
+                            'person_b_has_merkzeichen_h']
+        if not v and not any([values.get(merkzeichen_key) for merkzeichen_key in merkzeichen_keys]):
+            raise ValueError
+        return v
+
         
 class StepPauschbetragPersonB(StepPauschbetrag):
     name = 'person_b_requests_pauschbetrag'
     section_link = SectionLink('mandatory_data', StepFamilienstand.name, _l('form.lotse.mandatory_data.label'))
 
     label = _l('form.lotse.person_b.request_pauschbetrag.label')
-    preconditions = [ShowPersonBPrecondition, HasDisabilityPersonBPrecondition]
+    preconditions = [ShowPersonBPrecondition, HasDisabilityPersonBPrecondition, HasMerkzeichenPersonBPrecondition]
         
     class InputForm(SteuerlotseBaseForm):
         person_b_requests_pauschbetrag = SelectField(
---
+++ webapp/client/cypress/integration/PauschbetragPersonA.spec.js
@@ -0,0 +1,159 @@
+describe("PauschbetragPersonA", () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  context("with no data", () => {
+    beforeEach(() => {
+      cy.visit("/lotse/step/person_a_requests_pauschbetrag");
+    });
+
+    it("Should redirect to has disability page", () => {
+      cy.location().should((loc) => {
+        expect(loc.pathname.toString()).to.contain(
+          "/lotse/step/has_disability_person_a"
+        );
+      });
+    });
+  });
+
+  context("with has disability no", () => {
+    beforeEach(() => {
+      cy.request("POST", "/testing/set_data/form_data", {
+        person_a_has_disability: "no",
+      });
+
+      cy.visit("/lotse/step/person_a_requests_pauschbetrag");
+    });
+
+    it("Should redirect to has disability page", () => {
+      cy.location().should((loc) => {
+        expect(loc.pathname.toString()).to.contain(
+          "/lotse/step/has_disability_person_a"
+        );
+      });
+    });
+  });
+
+  context("with no data for merkzeichen", () => {
+    beforeEach(() => {
+      cy.request("POST", "/testing/set_data/form_data", {
+        person_a_has_disability: "yes",
+      });
+
+      cy.visit("/lotse/step/person_a_requests_pauschbetrag");
+    });
+
+    it("Should redirect to merkzeichen page", () => {
+      cy.location().should((loc) => {
+        expect(loc.pathname.toString()).to.contain(
+          "/lotse/step/merkzeichen_person_a"
+        );
+      });
+    });
+  });
+
+  context("with merkzeichen data", () => {
+    beforeEach(() => {
+      cy.request("POST", "/testing/set_data/form_data", {
+        person_a_has_disability: "yes",
+        person_a_disability_degree: "20",
+        person_a_has_merkzeichen_g: true,
+      });
+
+      cy.visit("/lotse/step/person_a_requests_pauschbetrag");
+    });
+
+    it("Should not check radio button for label yes", () => {
+      cy.get("#person_a_requests_pauschbetrag-yes").should("not.be.checked");
+    });
+
+    it("Should not check radio button for label no", () => {
+      cy.get("#person_a_requests_pauschbetrag-no").should("not.be.checked");
+    });
+
+    it("Should stay on page and show error when submit button clicked", () => {
+      cy.get("button[type=submit]").click();
+      cy.url().should("include", "/lotse/step/person_a_requests_pauschbetrag");
+      cy.get("[role=alert][for=person_a_requests_pauschbetrag]").contains(
+        "Diese Angabe wird benötigt, um fortfahren zu können"
+      );
+    });
+  });
+
+  context("with person_a_requests_pauschbetrag yes", () => {
+    beforeEach(() => {
+      cy.request("POST", "/testing/set_data/form_data", {
+        person_a_has_disability: "yes",
+        person_a_disability_degree: "20",
+        person_a_has_merkzeichen_g: true,
+        person_a_requests_pauschbetrag: "yes",
+      });
+
+      cy.visit("/lotse/step/person_a_requests_pauschbetrag");
+    });
+
+    it("Should check radio button for label yes", () => {
+      cy.get("#person_a_requests_pauschbetrag-yes").should("be.checked");
+    });
+
+    it("Should not check radio button for label no", () => {
+      cy.get("#person_a_requests_pauschbetrag-no").should("not.be.checked");
+    });
+
+    it("Should link back to merkzeichen person a page", () => {
+      cy.get("a").contains("Zurück").click();
+      cy.url().should("include", "/lotse/step/merkzeichen_person_a");
+    });
+
+    it("Should link forward to telephone_number page", () => {
+      cy.get("button[type=submit]").click();
+      cy.url().should("include", "/lotse/step/telephone_number");
+    });
+  });
+
+  context("with person_a_requests_pauschbetrag no", () => {
+    beforeEach(() => {
+      cy.request("POST", "/testing/set_data/form_data", {
+        person_a_has_disability: "yes",
+        person_a_disability_degree: "20",
+        person_a_has_merkzeichen_g: true,
+        person_a_requests_pauschbetrag: "no",
+      });
+      cy.visit("/lotse/step/person_a_requests_pauschbetrag");
+    });
+
+    it("Should check radio button for label no", () => {
+      cy.get("#person_a_requests_pauschbetrag-no").should("be.checked");
+    });
+
+    it("Should not check radio button for label yes", () => {
+      cy.get("#person_a_requests_pauschbetrag-yes").should("not.be.checked");
+    });
+
+    it("Should link forward to telephone_number page", () => {
+      cy.get("button[type=submit]").click();
+      cy.url().should("include", "/lotse/step/telephone_number");
+    });
+  });
+
+  context("for joint taxes", () => {
+    beforeEach(() => {
+      cy.request("POST", "/testing/set_data/form_data", {
+        familienstand: "married",
+        familienstand_married_lived_separated: "no",
+        familienstand_confirm_zusammenveranlagung: true,
+        person_a_has_disability: "yes",
+        person_a_disability_degree: "20",
+        person_a_has_merkzeichen_g: true,
+        person_a_requests_pauschbetrag: "yes",
+      });
+      cy.visit("/lotse/step/person_a_requests_pauschbetrag");
+    });
+
+    it("Should link forward to person b page", () => {
+      cy.get("button[type=submit]").click();
+      cy.url().should("include", "/lotse/step/person_b");
+    });
+  });
+});
