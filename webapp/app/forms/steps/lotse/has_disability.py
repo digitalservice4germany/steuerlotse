6f6c4acf33886bb1e17c6d56e1da750f30863b13
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-01-03 18:52:49 +0100

    Delete unnecessarily doubled preconditions

--- webapp/app/forms/steps/lotse/has_disability.py
+++ webapp/app/forms/steps/lotse/has_disability.py
@@ -1,9 +1,8 @@
 from flask import render_template
 from flask_babel import lazy_gettext as _l, _, ngettext
 from flask_wtf.csrf import generate_csrf
-from pydantic import validator, ValidationError
+from pydantic import validator
 from pydantic.main import BaseModel
-from wtforms import ValidationError
 from wtforms.validators import InputRequired
 
 from app.forms import SteuerlotseBaseForm
@@ -99,32 +98,6 @@ class StepDisabilityPersonA(LotseFormSteuerlotseStep):
                                header_title=_('form.lotse.header-title'))
 
 
-class PersonAHasDisabilityPrecondition(BaseModel):
-    _step_to_redirect_to = StepDisabilityPersonA.name
-    _message_to_flash = _l('form.lotse.skip_reason.has_no_disability')
-
-    person_a_has_disability: str
-
-    @validator('person_a_has_disability', always=True)
-    def has_to_be_yes(cls, value):
-        if value != 'yes':
-            raise ValidationError
-        return value
-
-
-class PersonBHasDisabilityPrecondition(BaseModel):
-    _step_to_redirect_to = StepDisabilityPersonB.name
-    _message_to_flash = _l('form.lotse.skip_reason.has_no_disability')
-
-    person_b_has_disability: str
-
-    @validator('person_b_has_disability', always=True)
-    def has_to_be_yes(cls, value):
-        if value != 'yes':
-            raise ValidationError
-        return value
-
-
 class HasDisabilityPersonAPrecondition(BaseModel):
     _step_to_redirect_to = StepDisabilityPersonA.name
     _message_to_flash = _l('form.lotse.skip_reason.has_no_disability')
@@ -134,7 +107,7 @@ class HasDisabilityPersonAPrecondition(BaseModel):
     @validator('person_a_has_disability', always=True)
     def has_to_be_set_true(cls, v):
         if not v:
-            raise ValidationError
+            raise ValueError
         return v
 
 
@@ -147,5 +120,5 @@ class HasDisabilityPersonBPrecondition(BaseModel):
     @validator('person_b_has_disability', always=True)
     def has_to_be_set_true(cls, v):
         if not v:
-            raise ValidationError
+            raise ValueError
         return v
