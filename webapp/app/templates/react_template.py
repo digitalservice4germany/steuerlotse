a6931d893a1e83cf0c412e72fb53b9251225a822
Sanny Nguyen Hung <sanny.nguyen@4germany.org>
2022-01-18 16:42:49 +0100

    Removed constant and add additional parameters

--- webapp/app/templates/react_template.py
+++ webapp/app/templates/react_template.py
@@ -1,10 +1,9 @@
 from flask import render_template
 
-REACT_COMPONENT = 'react_component.html'
-
-def render_react_template(component, props=None, form=None, header_title=None):                          
-    return render_template(REACT_COMPONENT,
+def render_react_template(component, props=None, form=None, header_title=None, **kwargs):
+    return render_template('react_component.html',
             component=component,
             props=props,
             form=form,
-            header_title=header_title)
+            header_title=header_title,
+            **kwargs)
