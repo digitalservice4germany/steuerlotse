8eaab7335c93a9d96b83e9a5138a435463d7699f
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-01-20 08:00:39 +0100

    STL-1490 | Page test redesigned

--- webapp/client/src/pages/SessionNotePage.test.js
+++ webapp/client/src/pages/SessionNotePage.test.js
@@ -7,22 +7,30 @@ const EXPECTED_HEADER = {
   title: "Hinweis zur Sitzung",
 };
 
-describe("with default props", () => {
-  beforeEach(() => {
-    const defaultProps = {
-      form: {
-        ...StepFormDefault.args,
-      },
-      prevUrl: "/some/prev/path",
-    };
-    render(<SessionNotePage {...defaultProps} />);
-  });
+const DEFAULT_PROPS = {
+  form: {
+    ...StepFormDefault.args,
+  },
+  prevUrl: "/some/prev/path",
+};
 
+const OVERVIEW_PROPS = {
+  form: {
+    action: "#form-submit",
+    csrfToken: "abc123imacsrftoken",
+    showOverviewButton: true,
+  },
+  prevUrl: "/some/prev/path",
+};
+
+describe("SessionNotePage", () => {
   it("should render step title text", () => {
+    render(<SessionNotePage {...DEFAULT_PROPS} />);
     expect(screen.getByText(EXPECTED_HEADER.title)).toBeInTheDocument();
   });
 
   it("should link to the previous page", () => {
+    render(<SessionNotePage {...DEFAULT_PROPS} />);
     expect(screen.getByText("Zurück").closest("a")).toHaveAttribute(
       "href",
       expect.stringContaining("/some/prev/path")
@@ -30,45 +38,18 @@ describe("with default props", () => {
   });
 
   it("should render a next button", () => {
+    render(<SessionNotePage {...DEFAULT_PROPS} />);
     expect(screen.getByText("Weiter")).toBeInTheDocument();
     expect(screen.getByText("Weiter").tagName).toEqual("BUTTON");
   });
 
   it("should not render a button to go to the overview", () => {
+    render(<SessionNotePage {...DEFAULT_PROPS} />);
     expect(screen.queryByText("Zurück zur Übersicht")).not.toBeInTheDocument();
-  });
-});
-
-describe("with overview props", () => {
-  beforeEach(() => {
-    const overviewProps = {
-      form: {
-        action: "#form-submit",
-        csrfToken: "abc123imacsrftoken",
-        showOverviewButton: true,
-      },
-      prevUrl: "/some/prev/path",
-    };
-    render(<SessionNotePage {...overviewProps} />);
-  });
-
-  it("should render step title text", () => {
-    expect(screen.getByText(EXPECTED_HEADER.title)).toBeInTheDocument();
-  });
-
-  it("should link to the previous page", () => {
-    expect(screen.getByText("Zurück").closest("a")).toHaveAttribute(
-      "href",
-      expect.stringContaining("/some/prev/path")
-    );
-  });
-
-  it("should render a next button", () => {
-    expect(screen.getByText("Weiter")).toBeInTheDocument();
-    expect(screen.getByText("Weiter").tagName).toEqual("BUTTON");
   });
 
   it("should render a button to go to the overview", () => {
+    render(<SessionNotePage {...OVERVIEW_PROPS} />);
     expect(screen.getByText("Zurück zur Übersicht")).toBeInTheDocument();
     expect(screen.getByText("Zurück zur Übersicht").tagName).toEqual("BUTTON");
   });
