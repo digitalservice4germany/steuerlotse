0c92f2adc378b248f8b74c447ce1d9a2f5bea8b8
Sanny Nguyen Hung <sanny.nguyen@4germany.org>
2022-02-15 23:15:37 +0100

    Added some debug informations

--- webapp/app/data_access/storage/session_storage.py
+++ webapp/app/data_access/storage/session_storage.py
@@ -1,3 +1,5 @@
+import logging
+
 from typing import Optional
 
 from pydantic import MissingError
@@ -10,19 +12,23 @@ from app.data_access.storage.cookie_storage import CookieStorage
 from app.config import Config
 
 
+logger = logging.getLogger(__name__)
 class SessionStorage(FormStorage):
     @staticmethod
     def get_data(data_identifier, ttl: Optional[int] = None, default_data=None):
         if Config.USE_COOKIE_STORAGE:
+            logger.debug(f"USE_COOKIE_STORAGE used!")
             return CookieStorage.get_data(data_identifier=data_identifier, ttl=ttl, default_data=default_data)
         
         
         key = SessionStorage.create_key_identifier_with_user_id(data_identifier)
+        logger.debug(f"Session Key generated for get: {key}")
         if not key:
             return None
         try:
             form_data = RedisConnectorService().get_from_redis(key)
         except MissingError:
+            logger.debug(f"MissingError from redis for key {key}")
             form_data = {}
 
         if default_data:
@@ -36,10 +42,12 @@ class SessionStorage(FormStorage):
     @staticmethod
     def override_data(stored_data, data_identifier='form_data'):
         if Config.USE_COOKIE_STORAGE:
+            logger.debug(f"USE_COOKIE_STORAGE used!")
             CookieStorage.override_data(data_to_store=stored_data, data_identifier=data_identifier)
             return
         
         key = SessionStorage.create_key_identifier_with_user_id(data_identifier)
+        logger.debug(f"Session Key generated for set: {key}")
         if not key:
             return
         RedisConnectorService().save_to_redis(key, SessionStorage.serialize_data(stored_data))
