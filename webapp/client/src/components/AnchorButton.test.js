a08912864aabe6dfcde7d1f26c975ee45673e237
Sanny Nguyen Hung <sanny.nguyen@4germany.org>
2022-02-15 17:22:48 +0100

    Add test for download attribute

--- webapp/client/src/components/AnchorButton.test.js
+++ webapp/client/src/components/AnchorButton.test.js
@@ -5,6 +5,7 @@ import AnchorButton from "./AnchorButton";
 const MOCK_PROPS = {
   text: "anchor text",
   url: "/some/link/path",
+  isDownloadLink: false,
 };
 
 describe("AnchorButton", () => {
@@ -15,6 +16,23 @@ describe("AnchorButton", () => {
     expect(screen.getByText(MOCK_PROPS.text).closest("a")).toHaveAttribute(
       "href",
       expect.stringContaining(MOCK_PROPS.url)
+    );
+  });
+
+  it("should not has a download attribute", () => {
+    render(<AnchorButton {...MOCK_PROPS} />);
+
+    expect(screen.getByText(MOCK_PROPS.text).closest("a")).not.toHaveAttribute(
+      "download"
+    );
+  });
+
+  it("should has a download attribute", () => {
+    MOCK_PROPS.isDownloadLink = true;
+    render(<AnchorButton {...MOCK_PROPS} />);
+
+    expect(screen.getByText(MOCK_PROPS.text).closest("a")).toHaveAttribute(
+      "download"
     );
   });
 });
