b9066fe71d907e299bbeb84bb31bb2222300d256
Sanny Nguyen Hung <sanny.nguyen@4germany.org>
2022-02-16 09:57:26 +0100

    Make tests independent

--- webapp/client/src/components/AnchorButton.test.js
+++ webapp/client/src/components/AnchorButton.test.js
@@ -5,7 +5,6 @@ import AnchorButton from "./AnchorButton";
 const MOCK_PROPS = {
   text: "anchor text",
   url: "/some/link/path",
-  isDownloadLink: false,
 };
 
 describe("AnchorButton", () => {
@@ -19,17 +18,18 @@ describe("AnchorButton", () => {
     );
   });
 
-  it("should not has a download attribute", () => {
-    render(<AnchorButton {...MOCK_PROPS} />);
+  it("should not have a download attribute", () => {
+    const PROPS_DATA = { ...MOCK_PROPS, isDownloadLink: false };
+    render(<AnchorButton {...PROPS_DATA} />);
 
     expect(screen.getByText(MOCK_PROPS.text).closest("a")).not.toHaveAttribute(
       "download"
     );
   });
 
-  it("should has a download attribute", () => {
-    MOCK_PROPS.isDownloadLink = true;
-    render(<AnchorButton {...MOCK_PROPS} />);
+  it("should have a download attribute", () => {
+    const PROPS_DATA = { ...MOCK_PROPS, isDownloadLink: true };
+    render(<AnchorButton {...PROPS_DATA} />);
 
     expect(screen.getByText(MOCK_PROPS.text).closest("a")).toHaveAttribute(
       "download"
