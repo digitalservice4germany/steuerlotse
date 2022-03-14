16f52bd07dd0f338571c1c51accaec332733e2d8
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-03-14 11:03:51 +0100

    STL-1934 | Added jest test for plausible goals

--- webapp/client/src/components/ShareIcons.test.js
+++ webapp/client/src/components/ShareIcons.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import ShareIcons from "./ShareIcons";
+import { fireEvent } from "@testing-library/dom";
 
 describe("ShareIcons no plausible", () => {
   const MOCK_PROPS_DEFAULT = {
@@ -24,5 +25,34 @@ describe("ShareIcons no plausible", () => {
 
   it("should not render whatsapp icon", () => {
     expect(screen.queryByLabelText("whatsapp")).not.toBeInTheDocument();
+  });
+});
+
+describe("ShareIcons with plausible", () => {
+  const MOCK_PROPS_PLAUSIBLE = {
+    promoteUrl: "www.google.com",
+    shareText: "Hey such an amazing search machine",
+    mailSubject: "Did you know it already?",
+    sourcePage: "ShareIcons.test",
+    plausibleDomain: "http://localhost:3000",
+  };
+
+  beforeEach(() => {
+    window.plausible = jest.fn().mockReturnValue({ plausible: jest.fn() });
+    render(<ShareIcons {...MOCK_PROPS_PLAUSIBLE} />);
+  });
+
+  it("should add plausible goal for facebook icon", () => {
+    fireEvent.click(screen.getByLabelText("facebook"));
+    expect(window.plausible).toHaveBeenCalledWith("Facebook icon clicked", {
+      method: MOCK_PROPS_PLAUSIBLE.sourcePage,
+    });
+  });
+
+  it("should add plausible goal for email icon", () => {
+    fireEvent.click(screen.getByLabelText("email"));
+    expect(window.plausible).toHaveBeenCalledWith("Email icon clicked", {
+      method: MOCK_PROPS_PLAUSIBLE.sourcePage,
+    });
   });
 });
