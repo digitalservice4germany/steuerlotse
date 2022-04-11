f94e5b721a71f735431bf8a3ea26fa87983e1146
Sanny Nguyen Hung <sanny.nguyen@4germany.org>
2022-04-11 07:06:55 +0200

    Add environment to task

--- webapp/app/config.py
+++ webapp/app/config.py
@@ -112,8 +112,7 @@ class FunctionalTestingConfig(DevelopmentConfig):
 
     ERICA_BASE_URL = 'ERICA'
     RATELIMIT_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite:///functional-testing.db"
-    SESSION_DATA_STORAGE_URL = 'redis://0.0.0.0:6379'
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite:///dev.db"
 
 class TestingConfig(BaseConfig):
     DEBUG = True
