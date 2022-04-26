c9dc7869c3413bf2a3d34ceda37f36c996c6738d
Tarek Hamaoui <tarek.m.hamaoui@gmail.com>
2022-04-26 07:34:07 +0200

    *adds stepheader component to the page *imports "anchorList" and "anchorBack" from the "contentPagesAnchorList" that Jordis made *adds mapping over the list items to make the whole page easier to read *adds the infoBox component (commented out). Waiting for Jordis PR merge.

--- webapp/client/src/pages/VorsorgeaufwendungenPage.js
+++ webapp/client/src/pages/VorsorgeaufwendungenPage.js
@@ -1,8 +1,7 @@
-import PropTypes from "prop-types";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import StepHeaderButtons from "../components/StepHeaderButtons";
-import anchorList from "../lib/contentPagesAnchorList";
+import { anchorList, anchorBack } from "../lib/contentPagesAnchorList";
 // import InfoBox from "../components/InfoBox";
 import {
   ContentWrapper,
@@ -14,7 +13,7 @@ import {
   Paragraph,
 } from "../components/ContentPagesGeneralStyling";
 
-export default function VorsorgeaufwendungenPage({ prevUrl }) {
+export default function VorsorgeaufwendungenPage() {
   const { t } = useTranslation();
 
   const anchorListItemsMap = anchorList
@@ -25,37 +24,65 @@ export default function VorsorgeaufwendungenPage({ prevUrl }) {
       </AnchorListItem>
     ));
 
+  const listDeductables = [
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph2.ListItem1"),
+    },
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph2.ListItem2"),
+    },
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph2.ListItem3"),
+    },
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph2.ListItem4"),
+    },
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph2.ListItem5"),
+    },
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph2.ListItem6"),
+    },
+  ];
+
+  const listNonDeductables = [
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph3.ListItem1"),
+    },
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph3.ListItem2"),
+    },
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph3.ListItem3"),
+    },
+    {
+      text: t("Vorsorgeaufwendungen.Paragraph3.ListItem4"),
+    },
+  ];
+
+  const listDeductablesItemsMap = listDeductables.map((item) => (
+    <ListItem key={item.text}>{item.text}</ListItem>
+  ));
+
+  const listNonDeductablesItemsMap = listNonDeductables.map((item) => (
+    <ListItem key={item.text}>{item.text}</ListItem>
+  ));
+
   return (
     <>
-      <StepHeaderButtons />
       <ContentWrapper>
+        <StepHeaderButtons text={anchorBack.text} url={anchorBack.url} />
         <Headline1>{t("Vorsorgeaufwendungen.Paragraph1.Heading")}</Headline1>
         <Paragraph>{t("Vorsorgeaufwendungen.Paragraph1.Text")}</Paragraph>
         <Headline2>{t("Vorsorgeaufwendungen.Paragraph2.Heading")}</Headline2>
-        <List>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph2.ListItem1")}</ListItem>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph2.ListItem2")}</ListItem>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph2.ListItem3")}</ListItem>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph2.ListItem4")}</ListItem>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph2.ListItem5")}</ListItem>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph2.ListItem6")}</ListItem>
-        </List>
+        <List>{listDeductablesItemsMap}</List>
         <Headline2>{t("Vorsorgeaufwendungen.Paragraph3.Heading")}</Headline2>
         <Paragraph>{t("Vorsorgeaufwendungen.Paragraph3.Text")}</Paragraph>
-        <List>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph3.ListItem1")}</ListItem>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph3.ListItem2")}</ListItem>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph3.ListItem3")}</ListItem>
-          <ListItem>{t("Vorsorgeaufwendungen.Paragraph3.ListItem4")}</ListItem>
-        </List>
+        <List>{listNonDeductablesItemsMap}</List>
         <Headline2>{t("Vorsorgeaufwendungen.Paragraph4.Heading")}</Headline2>
         <List>{anchorListItemsMap}</List>
       </ContentWrapper>
-      {/* <InfoBox fscRequestUrl={fscRequestUrl} /> */}
+      {/* <InfoBox /> */}
     </>
   );
 }
-
-VorsorgeaufwendungenPage.propTypes = {
-  prevUrl: PropTypes.string.isRequired,
-};
