d367303e2801eaa5ad71f49b7b74093d366d2142
jo punknoir <joerdis.schuller@digitalservice.bund.de>
2022-04-29 11:52:33 +0200

    refactor overview page

--- webapp/client/src/components/ContentPagesGeneralStyling.js
+++ webapp/client/src/components/ContentPagesGeneralStyling.js
@@ -3,11 +3,12 @@ import styled from "styled-components";
 export const ContentWrapper = styled.div`
   padding-left: var(--spacing-03);
   padding-right: var(--spacing-03);
-  margin: 0 var(--spacing-03);
+  margin: 0 auto;
   max-width: var(--content-pages-max-width);
 
-  @media (min-width: 1224px) {
-    margin: 0 var(--spacing-12);
+  @media (min-width: 1025px) {
+    padding-left: var(--spacing-10);
+    margin: 0 auto;
   }
 `;
 
