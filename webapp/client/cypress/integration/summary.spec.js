809be690f16f127bc170fd84fc0c1789eaed8b22
Sanny Nguyen Hung <sanny.nguyen@4germany.org>
2022-05-11 17:08:59 +0200

    Revert "Fix tests for build"
    This reverts commit fa96dd327668b6648c384165afc0da4b006c92cf.

--- webapp/client/cypress/integration/summary.spec.js
+++ webapp/client/cypress/integration/summary.spec.js
@@ -6,7 +6,7 @@ describe("Summary", () => {
 
     it("shows alert message for page", () => {
       cy.visit("/lotse/step/summary");
-      cy.get("div.alert[role=alert]").should("have.length", 2);
+      cy.get("div.alert[role=alert]").should("have.length", 1);
     });
   });
 
@@ -48,16 +48,14 @@ describe("Summary", () => {
       });
     });
 
-    /* REVERT THIS! */
-
-    /* it("does not show alert message for field", () => {
+    it("does not show alert message for field", () => {
       cy.get("[name=next_button]").click();
       cy.get("div.invalid-feedback[role=alert]").should("not.exist");
-    }); */
+    });
 
     it("shows alert message for page once", () => {
       cy.get("[name=next_button]").click();
-      cy.get("div.alert[role=alert]").should("have.length", 2);
+      cy.get("div.alert[role=alert]").should("have.length", 1);
     });
   });
 });
