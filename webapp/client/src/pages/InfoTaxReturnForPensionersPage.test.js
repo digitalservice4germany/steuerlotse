5ca07c9bf057d12c84a75a5ca6c3e2cf1056bcee
Tarek Hamaoui <tarek.m.hamaoui@gmail.com>
2022-05-16 07:34:14 +0200

    *adds tests for new contentPageBox component *adds tests for infoTaxReturnForPensionersPage

--- webapp/client/src/pages/InfoTaxReturnForPensionersPage.test.js
+++ webapp/client/src/pages/InfoTaxReturnForPensionersPage.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import InfoTaxReturnForPensionersPage from "./InfoTaxReturnForPensionersPage";
 
 jest.mock("../components/ContentPageBox", () => ({
@@ -26,7 +26,37 @@ function setup() {
 }
 
 describe("InfoTaxReturnForPensionersPage", () => {
-  it("should render the InfoTaxReturnForPensionersPage component", () => {
+  it("should render the InfoTaxReturnForPensionersPage page", () => {
     setup();
+
+    const headline1 = screen.getByText(
+      "Vereinfachte Steuererklärung für Rentner — wie Sie in wenigen Minuten Ihre Steuererklärung kostenlos online machen können"
+    );
+    const headline2 = screen.getByText(
+      "Die vereinfachte Steuererklärung wurde speziell für Menschen im Ruhestand entwickelt"
+    );
+    const headline3 = screen.getByText(
+      "Wie finde ich heraus, ob ich als Rentner steuerpflichtig bin?"
+    );
+    const headline4 = screen.getByText(
+      "So funktioniert der Steuerlotse für Rente und Pension"
+    );
+
+    expect(headline1).toBeInTheDocument();
+    expect(headline2).toBeInTheDocument();
+    expect(headline3).toBeInTheDocument();
+    expect(headline4).toBeInTheDocument();
+  });
+
+  it("should render the ContentPageBox component", () => {
+    setup();
+
+    expect(screen.getByText("Content Page Box")).toBeInTheDocument();
+  });
+
+  it("should render the SuccessStepsInfoBox component", () => {
+    setup();
+
+    expect(screen.getByText("Steps Info Box")).toBeInTheDocument();
   });
 });
