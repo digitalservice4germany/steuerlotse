4583e00bcf84a724008d64af4e55a8dc29913ce3
Julian <julian_roesner@gmx.de>
2022-05-19 13:20:14 +0200

    Add more http header (#901)

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -146,7 +146,18 @@ def register_request_handlers(app):
         if not Config.SET_SECURITY_HTTP_HEADERS:
             return response
 
-        response.headers['X-Content-Type-Options'] = 'no-sniff'
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+        response.headers['Referrer-Policy'] = 'same-origin'
+        response.headers['Permissions-Policy'] = 'Permissions-Policy: accelerometer=(), ambient-light-sensor=(), ' \
+                                                 'autoplay=(), battery=(), camera=(), cross-origin-isolated=(), ' \
+                                                 'display-capture=(), document-domain=(), encrypted-media=(), ' \
+                                                 'execution-while-not-rendered=(), execution-while-out-of-viewport=(' \
+                                                 '), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), ' \
+                                                 'magnetometer=(), microphone=(), midi=(), navigation-override=(), ' \
+                                                 'payment=(), picture-in-picture=(), publickey-credentials-get=(), ' \
+                                                 'screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), ' \
+                                                 'xr-spatial-tracking=() '
         response.headers['Content-Security-Policy'] = (
             "default-src 'self'; "
             "script-src 'self' 'unsafe-inline' plausible.io; "
