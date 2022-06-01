f1d1e70ed8a0d8e806a1c34688bf19b5e29603c9
Tarek Hamaoui <tarek.m.hamaoui@gmail.com>
2022-06-01 10:20:39 +0200

    *adds default for successStepsInfoBox and ContentPageBox

--- webapp/client/src/stories/SuccessStepsInfoBox.stories.js
+++ webapp/client/src/stories/SuccessStepsInfoBox.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SuccessStepsInfoBox from "../components/SuccessStepsInfoBox";
+import OneIcon from "../assets/icons/Icon-1.svg";
 
 export default {
   title: "Components/StepsInfoBox",
@@ -11,3 +12,14 @@ function Template(args) {
 }
 
 export const Default = Template.bind({});
+Default.args = {
+  header: "Vorbereiten und Belege sammeln",
+  text: "Sie können sich auf Ihre Steuererklärung vorbereiten, bis Sie den Brief erhalten haben. Eine Übersicht über die notwendigen Unterlagen und Informationen, die Sie für die Erstellung Ihrer Steuererklärung brauchen, finden Sie in unserer Vorbereitungshilfe oder unter dem Menüpunkt Vorbereiten.",
+  anchor: {
+    url: "/download_preparation",
+    text: "Vorbereitungshilfe speichern",
+  },
+  icon: {
+    iconSrc: OneIcon,
+  },
+};
